aeb19a5446ac0bb66b99cf1c2b21127b6f538025
crazie450-prog <crazie450@gmail.com>
2025-10-30 11:51:06 -0700

    added verbose opion

--- main.py
+++ main.py
@@ -9,18 +9,29 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
-    user_prompt = input_prompt()
+    # Get user prompt and verbosity setting
+    user_prompt, verbose = input_prompt()
+        
+    # Create message content for the LLM
     messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-    
-    
+
+    # Generate response from the LLM
     response = client.models.generate_content(
     model="gemini-2.0-flash-001", contents = messages
     )
+    
+    # Output
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    # Verbose output
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        
+
 
 
 if __name__ == "__main__":
--- utilities.py
+++ utilities.py
@@ -1,9 +1,10 @@
-import sys
+import argparse
 
 # Input Prompt
 def input_prompt():
-    if len(sys.argv) == 2:
-        return sys.argv[1]
-    else:
-        print("Usage: python3 main.py <prompt>")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description='CLI LLM')
+    parser.add_argument('prompt', type=str, help='The prompt to send to the LLM')
+    parser.add_argument('--verbose', action='store_true', help='Enable verbose output')
+
+    args = parser.parse_args()
+    return args.prompt, args.verbose
